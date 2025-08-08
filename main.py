4418fcc3850ce8378db8dc113856164d13dc8b9a
Eb1su <higginsjamie3@gmail.com>
2025-08-08 17:31:25 +0100

    added ai response functionality

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get('GEMINI_API_KEY')
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=sys.argv[1]
+)
+
+print(response.text)
+print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
