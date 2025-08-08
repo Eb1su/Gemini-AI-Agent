0e4226f35904aabe63aab31573345a2ac923ac8e
Eb1su <higginsjamie3@gmail.com>
2025-08-08 18:16:19 +0100

    improved codebase readability and added verbose checking

--- main.py
+++ main.py
@@ -2,16 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get('GEMINI_API_KEY')
-client = genai.Client(api_key=api_key)
+MODEL_NAME = 'gemini-2.0-flash-001'
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=sys.argv[1]
-)
+def main():
+    
+    user_prompt = sys.argv[1]
+    verbose = len(sys.argv) == 3 and sys.argv[2] == '--verbose'
 
-print(response.text)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    # API Initalization
+    load_dotenv()
+    api_key = os.environ.get('GEMINI_API_KEY')
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role='user', parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=messages,
+    )
+
+    if verbose:
+        print(f'User prompt: {user_prompt}\n')
+        print(response.text)
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    else:
+        print(response.text)
+
+if __name__ =='__main__':
+    main()
